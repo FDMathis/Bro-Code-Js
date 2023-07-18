dfc50bb1d9a7654a5880fce312e3db6c8816b9b0
FDMathis <111214276+FDMathis@users.noreply.github.com>
2023-07-18 10:09:54 -0600

    Functions

---
+++ Functions/index.js
@@ -0,0 +1,30 @@
+// Functions = Define code once, and use it many times
+//             To perform, call the function
+
+let userName = "Kaeden"
+
+function happyBirthday(){
+console.log("Happy Birthday to you!");
+console.log("Happy Birthday to you!");
+console.log("Happy Birthday, dear " + userName + ",");
+console.log("Happy Birthday to you!");
+}
+
+happyBirthday(userName)
+
+//Parameters must either be within the function or called from another function
+
+publicEnemy()
+
+function publicEnemy(){
+    who = "911"
+    where = "your town"
+}
+
+function isAJoke(who, where){
+    console.log("Get up...")
+    console.log("Get, Get, Get down...")
+    console.log(who + " is a joke in " + where + ".")
+}
+
+isAJoke(who, where)
