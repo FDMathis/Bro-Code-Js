70f001405a7ff2d980eedfb4e0de95e822405c9f
FDMathis <111214276+FDMathis@users.noreply.github.com>
2023-08-20 20:04:48 -0600

    Bro -timer

---
+++ console.time()/index.js
@@ -0,0 +1,14 @@
+// console.time() = Starts with a timer you can user to
+//                  track how long an operation takes
+//                  Give each timer a unique name.
+//                  Times synchronous operations, does not
+//                  time asynchronous operations
+
+
+//start
+console.time("Response time");
+
+alert("click the OK button!");
+
+//end
+console.timeEnd("Response time");
--- scratchpad/scratch.js
+++ scratchpad/scratch.js
@@ -1,3 +1,21 @@
 
-ans = (4 % 7);
-console.log(ans)
+class user{
+
+    constructor(firstName, lastName, birthdate, address,phoneNumber, chefStatus, kitchenName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthdate = birthdate;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.chefStatus = chefStatus;
+        this.kitchenName = kitchenName;
+    }
+}
+
+class menu{
+
+    constructor(item, price){
+        this.item = item;
+        this.price = price;
+    }
+}
