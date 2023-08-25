00687cc17a7fddda694346b956781993020038ab
FDMathis <111214276+FDMathis@users.noreply.github.com>
2023-08-25 12:32:00 -0600

    Bro Dom stuff

---
+++ DOM traversal/index.js
@@ -0,0 +1,13 @@
+
+let element = document.body;
+
+// .firstElementChild
+let child = element.firstElementChild;
+child.style.backgroundColor = "lightgreen";
+
+// .lastElementChild
+// .parentElement
+// .nextElementSibling
+// .previousElementSibling
+// .children[]
+// .Array.from(.children)
