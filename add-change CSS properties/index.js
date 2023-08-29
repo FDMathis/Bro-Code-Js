377d3c7de0dfd60cf2ba9f80d5aed89e9753afe7
FDMathis <111214276+FDMathis@users.noreply.github.com>
2023-08-28 20:37:49 -0600

    Bro add-change CSS

---
+++ add-change CSS properties/index.js
@@ -0,0 +1,19 @@
+// First, store the element in a variable
+const title = document.getElementById("myTitle");
+
+//Next, element+ style attribute+ CSS attribute
+//Background by color
+title.style.backgroundColor = "blue";
+//Background by hexidecimal
+title.style.backgroundColor = "#222222";
+//Background by rgb
+title.style.backgroundColor = "rgb(50, 200, 250";
+
+title.style.color = "purple";
+
+title.style.fontFamily = "consolas";
+title.style.textAlign = "center";
+title.style.border = "2px solid purple";
+
+//hide an element
+//title.style.display = "none";
