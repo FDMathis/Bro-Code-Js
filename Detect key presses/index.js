5ac969d097d4381fc6e0f7688ae6245dd7b48476
FDMathis <111214276+FDMathis@users.noreply.github.com>
2023-09-01 22:18:10 -0600

    bro-keystrokes

---
+++ Detect key presses/index.js
@@ -0,0 +1,33 @@
+
+const myDiv = document.getElementById("myDiv");
+
+//Shows keystrokes
+window.addEventListener("keydown", event => console.log(event.key));
+
+//Detects keystrokes to move something on screen
+window.addEventListener("keydown", move);
+let x = 0;
+let y = 0;
+
+function move(event){
+    switch(event.key){
+        case "ArrowDown":
+            y+=5;
+            myDiv.style.top = y + "px";
+            break;
+        case "ArrowUp":
+            y-=5;
+            myDiv.style.top = y + "px";
+            break;
+        case "ArrowLeft":
+            x-=5;
+            myDiv.style.left = x + "px";
+            break;
+        case "ArrowRight":
+            x+=5;
+            myDiv.style.left = x + "px";
+            break;   
+        default:
+            break; 
+    }
+}
